a69247fc1ea1e4405a2e74434e6847619808bd85
Sena Demirbaş <201307018@kocaeli.edu.tr>
2023-12-09 18:48:46 +0300

    connected to database

--- server/server.js
+++ server/server.js
@@ -1,8 +1,34 @@
-const express = require("express");
+import mysql2 from "mysql2";
+import express from "express";
+
 const app = express();
 
-app.get("/api", (req, res) => {
-  res.json({ users: ["userOne", "userTwo", "userThree"] });
+const db = mysql2.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "sena123",
+  database: "bitirme",
+});
+app.get("/", (req, res) => {
+  res.json("hello this is backend");
+});
+
+app.get("/users", (req, res) => {
+  const user = "SELECT * FROM users";
+  db.query(user, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
+
+app.post("/users", (req, res) => {
+  const user = "INSERT INTO users (`user_name`,`user_mail`) VALUES (?)";
+  const values = ["can", "cansaginc@hotmail.com"];
+
+  db.query(user, [values], (err, data) => {
+    if (err) return res.json(err);
+    return res.json("New users added");
+  });
 });
 
 app.listen(5000, () => {
